a76d13990d3cbbd17f7df482d1526686c9b87a92
faisalthaheem <>
2021-05-02 14:01:32 +0200

    Added Message 301

---
+++ stanag4586edav1/message301.py
@@ -0,0 +1,48 @@
+"""
+ Copyright (c) 2021 Faisal Thaheem (https://github.com/faisalthaheem/python-stanag-4586-EDA-v1)
+ License GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+"""
+
+from ctypes import *
+from pprint import pprint
+from .message_base import MessageBase
+
+MSG301_NULL = b"\x00"*63
+
+class Message301(BigEndianStructure, MessageBase):
+    _pack_ = 1
+    _fields_ = [
+        ("time_stamp",                          c_double),
+        ("vehicle_id",                          c_int32),
+        ("cucs_id",                             c_int32),
+        ("vsm_id",                              c_int32),
+        ("station_number",                      c_uint32),
+        ("eo_ir_type",                          c_char*14),
+        ("eo_ir_type_revision_level",           c_ubyte),
+        ("eo_vertical_image_dimension",         c_int16),
+        ("eo_horizontal_image_dimension",       c_int16),
+        ("ir_vertical_image_dimension",         c_int16),
+        ("ir_horizontal_image_dimension",       c_int16),
+        ("field_of_regard_elevation_min",       c_float),
+        ("field_of_regard_elevation_max",       c_float),
+        ("field_of_regard_azimuth_min",         c_float),
+        ("field_of_regard_azimuth_max",         c_float),
+      ]
+    
+    def __new__(cls, byte_buffer=None):
+        return cls.from_buffer_copy(byte_buffer)
+
+    def __init__(self, byte_buffer=None):
+        pass
+
+    def encode(self):
+        return bytes(self)
+
+    def dump(self):
+        pprint(self)
+
+    def get_eo_ir_type(self):
+        return self.get_string_field("eo_ir_type")
+
+    def set_eo_ir_type(self, val):
+        return self.set_string_field("eo_ir_type", 14, val)
---
+++ tests/test_message301.py
@@ -0,0 +1,84 @@
+"""
+ Copyright (c) 2021 Faisal Thaheem (https://github.com/faisalthaheem/python-stanag-4586-EDA-v1)
+ License GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+"""
+
+from stanag4586edav1.message301 import *
+
+
+PACKET_TO_DECODE = b"\x00\x00\x00\x00\x00\x00\x00\x00" \
+b"\x00\x00\x00\x50" \
+b"\x00\x00\x00\xA0" \
+b"\x00\x00\x00\x00" \
+b"\x00\x00\x00\x08" \
+b"\x31\x31\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" \
+b"\x01" \
+b"\x03\x00" \
+b"\x04\x00" \
+b"\x03\x00" \
+b"\x04\x00" \
+b"\x3f\x80\x00\x00" \
+b"\x3f\x80\x00\x00" \
+b"\x3f\x80\x00\x00" \
+b"\x3f\x80\x00\x00" 
+
+def test_decode_message301():
+    msg300 = Message301(PACKET_TO_DECODE)
+    
+    assert msg300.time_stamp == 0x00
+    assert msg300.vehicle_id == 0x50
+    assert msg300.cucs_id == 0xA0
+    assert msg300.vsm_id == 0x00
+    assert msg300.station_number == 0x08
+    assert msg300.eo_ir_type == b"\x31\x31"
+    assert msg300.eo_ir_type_revision_level == 0x01
+    assert msg300.eo_vertical_image_dimension == 768
+    assert msg300.eo_horizontal_image_dimension == 1024
+    assert msg300.ir_vertical_image_dimension == 768
+    assert msg300.ir_horizontal_image_dimension == 1024
+    assert msg300.field_of_regard_elevation_min == 1.0
+    assert msg300.field_of_regard_elevation_max == 1.0
+    assert msg300.field_of_regard_azimuth_min == 1.0
+    assert msg300.field_of_regard_azimuth_max == 1.0
+
+
+def test_encode_message301():
+    msg301 = Message301(MSG301_NULL)
+
+    msg301.time_stamp = 0x00
+    msg301.vehicle_id = 0x50
+    msg301.cucs_id = 0xA0
+    msg301.vsm_id = 0x00
+    msg301.station_number = 0x08
+    msg301.eo_ir_type = b"\x31\x31"
+    msg301.eo_ir_type_revision_level = 0x01
+    msg301.eo_vertical_image_dimension = 768
+    msg301.eo_horizontal_image_dimension = 1024
+    msg301.ir_vertical_image_dimension = 768
+    msg301.ir_horizontal_image_dimension = 1024
+    msg301.field_of_regard_elevation_min = 1.0
+    msg301.field_of_regard_elevation_max = 1.0
+    msg301.field_of_regard_azimuth_min = 1.0
+    msg301.field_of_regard_azimuth_max = 1.0
+
+    assert msg301.encode() == PACKET_TO_DECODE
+
+def test_set_get_eo_ir_type():
+    msg301 = Message301(MSG301_NULL)
+
+    ID_TO_TEST = "valid id"
+    assert msg301.set_eo_ir_type(ID_TO_TEST) == True
+
+    assert msg301.get_eo_ir_type() == ID_TO_TEST
+
+def test_set_get_eo_ir_type_exceeding_length():
+    msg301 = Message301(MSG301_NULL)
+
+    ID_TO_TEST = "valid id valid id valid id valid id"
+    assert msg301.set_eo_ir_type(ID_TO_TEST) == False
+
+def test_set_get_eo_ir_type_zero_length():
+    msg301 = Message301(MSG301_NULL)
+
+    ID_TO_TEST = ""
+    assert msg301.set_eo_ir_type(ID_TO_TEST) == False
